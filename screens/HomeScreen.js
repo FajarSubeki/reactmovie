2c5e90c91eb14f27493c7b9cb99feb33633a58bb
Fajar Subeki <fajar.subeki@work.bri.co.id>
2025-02-19 09:07:47 +0700

    add header section

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,25 @@
-import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Platform, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Bars3CenterLeftIcon, MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import { styles } from "../theme";
 
+const ios = Platform.OS == 'ios';
 export default function HomeScreen() {
     return (
         <View className="flex-1 bg-neutral-800">
-            <Text className="text-red-500">Home Screen</Text>
+            <SafeAreaView className={ios? "-mb-2": "mb-3"}>
+                <StatusBar style="light" />
+                <View className="flex-row justify-between items-center mx-4">
+                    <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white"/>
+                    <Text className="text-white text-3xl font-bold">
+                        <Text style={styles.text}>M</Text>ovies
+                    </Text>
+                    <TouchableOpacity>
+                        <MagnifyingGlassIcon size="30" strokeWidth={2} color="white" />
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
         </View>
     )
 }
