8b1d1b7119682df5930ef0e58882289d35939065
Fajar Subeki <fajar.subeki@work.bri.co.id>
2025-02-24 19:25:06 +0700

    add seach movie

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,22 +1,45 @@
 import { View, Text, Dimensions, TextInput, TouchableOpacity, ScrollView, TouchableWithoutFeedback, Image } from 'react-native'
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation } from '@react-navigation/native'
 import { XMarkIcon } from 'react-native-heroicons/outline'
 import Loading from '../components/loading';
+import { fallbackMoviePoster, image185, searchMovies } from '../api/moviedb';
+import debounce from 'lodash/debounce';
 
 const {width, height} = Dimensions.get('window')
 
 export default function SearchScreen() {
     const navigation = useNavigation()
-    const [results, setResult] = useState([1,2,3,4])
+    const [results, setResults] = useState([])
     let movieName = 'Avengers Revenge'
     const [loading, setLoading] = useState(false)
+    const handleSearch = value=>{
+        if(value && value.length>2) {
+            setLoading(true)
+            searchMovies({
+                query: value,
+                include_adult: 'false',
+                language: 'en-US',
+                page: '1'
+            }).then(data=>{
+                setLoading(false)
+                console.log('get movies', data)
+                if(data && data.results) setResults(data.results)
+            })
+        } else {
+            setLoading(false)
+            setResults([])
+        }
+    }
+
+    const handleTextDebounce = useCallback(debounce(handleSearch, 400), [])
 
   return (
     <SafeAreaView className="bg-neutral-800 flex-1">
         <View className="mx-4 mb-3 flex-row justify-between items-center border border-neutral-500 rounded-full">
             <TextInput
+                onChangeText={handleTextDebounce}
                 placeholder='Search Movie'
                 placeholderTextColor={'lightgray'}
                 className="pb-1 pl-6 flex-1 text-base font-semibold text-white tracking-wider" />
@@ -46,10 +69,11 @@ export default function SearchScreen() {
                                             onPress={() => navigation.push("Movie", item)}>
                                             <View className="space-y-3 mb-4 items-center">
                                                 <Image className="rounded-3xl"
-                                                    source={require('../assets/images/avengers.jpg')}
+                                                    // source={require('../assets/images/avengers.jpg')}
+                                                    source={{uri: image185(item?.poster_path) || fallbackMoviePoster}}
                                                     style={{width: width*0.44, height: height*0.3}} />
                                                 <Text className="text-neutral-300 ml-1 mt-2">
-                                                    {movieName.length>22 ? movieName.slice(0,22)+'...': movieName }
+                                                    {item?.title.length>22 ? item?.title.slice(0,22)+'...': item?.title }
                                                 </Text>
                                             </View>
                                         </TouchableWithoutFeedback>
